37966bbedfa6ce6b541b5a82f8113b24118413ba
anuradha931 <anuradhakommineni2@gmail.com>
2020-01-26 20:46:36 -0500

    servlet example

---
+++ src/main/java/com/servlet/examples/LoginServlet.java
@@ -0,0 +1,23 @@
+package com.servlet.examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request,response);
+		
+
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Welcome From JSP</title>
+</head>
+<body>
+   
+    <form action="/login.do" method="post">
+    Enter Your Name : <input type="text" name="name" />
+    <input type="submit" value="login" />
+    
+    </form>
+</body>
+</html>
